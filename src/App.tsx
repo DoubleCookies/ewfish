e050e17ba2086898fc7b1648446569cec4c5ff21
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-07-27 13:20:57 +0300

    test infinite scrolling pictures

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import './App.css';
 import GithubIcon from "./GithubIcon";
+import ew0 from './images/ewfish/ew1-1.png';
+import ew1 from './images/ewfish/ew1-2.png';
+import ew2 from './images/ewfish/ew2.jpg';
+import ew3 from './images/ewfish/ew3.png';
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <div>
-         Ewfish<br/>༼ つ ◕_◕ ༽つ
+      <main>
+        <div className="scrollable-header">
+          <div className="animation">
+            <div className="animation-elements">
+              <img src={ew0} alt="ew0" />
+              <img src={ew1} alt="ew1" />
+              <img src={ew2} alt="ew2" />
+              <img src={ew3} alt="ew3" />
+              <img src={ew0} alt="ew0" />
+              <img src={ew1} alt="ew1" />
+              <img src={ew2} alt="ew2" />
+              <img src={ew3} alt="ew3" />
+              <img src={ew0} alt="ew0" />
+              <img src={ew1} alt="ew1" />
+              <img src={ew2} alt="ew2" />
+              <img src={ew3} alt="ew3" />
+            </div>
+          </div>
         </div>
-      </header>
-      <main className="main-content">
-        Ewfish
+        <div className="main-content">
+          Ewfish. Other cool things are coming soon!
+        </div>
+
       </main>
       <footer className="footer-block">
         <div>
