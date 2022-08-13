b9ddedbea477cdfb690bfb5388370663eb1ea1fc
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-08-13 09:39:38 +0300

    Sections divider styling

--- src/components/SectionsDivider/SectionsDivider.tsx
+++ src/components/SectionsDivider/SectionsDivider.tsx
@@ -2,7 +2,7 @@ import './SectionsDivider.css';
 
 function SectionsDivider(props: {title: string}) {
   return (
-    <div style={{width: '100%'}}>
+    <div className="divider-block" style={{width: '100%'}}>
       <hr className="section-divider" />
         <h1 className="section-header" style={{margin: 4}}>
           {props.title}
