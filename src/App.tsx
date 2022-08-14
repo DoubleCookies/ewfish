574ad507cde4a9c11ed41846ed1f8765e6233a12
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-08-14 16:12:24 +0300

    Links block

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import About from "./components/About/About";
 import Results from "./components/Results/Results";
 import SectionsDivider from "./components/SectionsDivider/SectionsDivider";
 import Footer from "./components/Footer/Footer";
+import Links from "./components/Links/Links";
 
 function App() {
   return (
     <div className="app">
-      <ScrollableHeader />
+      <ScrollableHeader/>
       <main>
         <div className="main-content">
-          <SectionsDivider title="Work in progress! More cool things coming soon!" />
-          <About />
-          <SectionsDivider title="Results" />
-          <Results />
+          <SectionsDivider title="Work in progress! More cool things coming soon!"/>
+          <About/>
+          <SectionsDivider title="Results"/>
+          <Results/>
+          <SectionsDivider title="Where you can find us:"/>
+          <Links/>
         </div>
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Links/Links.tsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons'
+import bg from "../../images/Backgrounds/bg1.jpg";
+import './Links.css';
+
+function Links() {
+  return(
+    <div className="anim-bg-block" style={{ width: '100%', backgroundImage: `url(${bg})` }}>
+      <div className="links-block">
+        <div className="links-row">
+          <a href="https://twitter.com/Ewfish_spl" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faTwitter} size={"4x"} className="twitter-icon" />
+          </a>
+          <b style={{fontSize: "1.4rem"}}>Twitter</b>
+          <p>
+            Here we post our results in tournaments, some memes or just repost cool things from team members.
+          </p>
+        </div>
+        <div className="links-row">
+          <a href="https://discord.com/invite/t2nxshYDYg" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faDiscord} size={"4x"} className="discord-icon" />
+          </a>
+          <b style={{fontSize: "1.4rem"}}>Discord</b>
+          <p>
+            We have Discord server where we plan our participation in events, discuss strategies and having good time!
+            Feel free to join us!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Links;
