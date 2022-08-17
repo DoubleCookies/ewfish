a91ae0e2685c229c23054ec6edada60d223ea63a
yaga <46373458+Yagaaa@users.noreply.github.com>
2022-08-17 15:10:46 +0300

    Corrections to the text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -17,63 +17,51 @@ function About() {
         </div>
         <div className="header-column">
           <h1 className="section-header">About</h1>
-          <i className="section-subHeader">How our team was formed, what did we do in the beginning and other
-            interesting stuff!</i>
+          <i className="section-subHeader">How our team was created, what we did in the beginning and other trivia!</i>
         </div>
       </div>
       <div className="about-row2">
         <div className="col-text">
           <span className="section-subHeader">2020</span>
           <ReactMarkdown className="about-text">
-            Our team was formed in October 2020 by *EW*KD and Li*fish*. Although initially the name was just a
-            placeholder it stuck around. Initially we participated exclusively in local russian tourneys
-            (we didn't do that good to be honest). All in all
-            2020 was a pretty rough year for the team (we didn't play play often, didn't get any solid results, EWKD
-            joined
-            another team).
+            Our team was created in October 2020 by EWKD and Lifish. Although initially the name was just a 
+            placeholder it stuck around. At first we participated exclusively in local russian tourneys (and didn’t do that hot). 
+            All in all 2020 was a pretty rough year for our team 
+            (we didn't play often as well as didn't get any solid results and EWKD ended up joining a different team).
           </ReactMarkdown>
           <span className="section-subHeader">2021</span>
-          <p className="about-text"> Nonetheless we continued to play and were improving as a team.
-            And then came time for <a href="https://challonge.com/ru/TheCupOfTea" target="_blank" rel="noreferrer">The
-              Cup Of Tea</a> —
-            one of the last major russian tourney.
+          <p className="about-text"> 
+          Nonetheless we continued to play and were improving as a team. Then came time for <a href="https://challonge.com/ru/TheCupOfTea" target="_blank" rel="noreferrer">The
+              Cup Of Tea</a> — one of the last major russian tourneys.
           </p>
           <ReactMarkdown className="about-text">
-            This tourney was held in march of 2021. It was typical Double Elimination but there were three in each
-            team
-            instead of the usual four (it's a really fun and fresh take on the formula). Right before this event
-            Ewfish
-            finally became, well, Ewfish (and *the first, the best, and the only* logo was created). And it was great
-            — we
-            almost got to the losers bracket finale! A few days after The Cup Of Tea, Chronya
-            (one of the strongest Splatoon ru-player) suggested to participate in LUTI and even help us as substitute.
-            We decided to try it out and that was certainly for the best!
+            This tourney was held in March 2021. It was a typical Double Elimination format but there was a twist - it was 3-on-3 instead of 4-on-4. 
+            Right before this event Ewfish finally became, well, Ewfish (and the first, the best, and the only logo was created). 
+            The tournament went great — we almost got to the losers bracket finale! 
+            A few days after The Cup Of Tea, Chronya (one of the strongest russian Splatoon players) suggested that we participate in LUTI 
+            and even offered to help us as a sub. 
+            We decided to try it out and that decision was certainly for the best!
           </ReactMarkdown>
-          <p className="about-text">After the seeding tourney we got into division 8 (as expected) and got 4th in group
-            though we were very close to
-            2nd and 3rd place teams. It was fun, challenging, engaging, and informative but that's besides the point,
-            the most
-            important part is that after the season ended our momentum didn't halt and we started taking part in more
-            tournaments!
+          <p className="about-text">
+            After the seeding tourney we got into division 8 (as expected) and got 4th in group (although we were very close to the 2nd and 3rd place teams). 
+            It was a really fun event but that's besides the point, the most important thing is that after the season ended our
+            momentum didn't stop and we began taking part in more tournaments!
           </p>
           <p className="about-text">
-            In this year we participated in many tournaments and got some prize places (1st place in LSL 13 Emerald
-            Bracket,
-            2nd place in LSL 15 Silver Bracket, 3rd place in URC x LoRule — local ru-tournament).
-          </p>
+            In 2021 we participated in many tourneys and got some good placements 
+            (1st place in LSL 13 Emerald Bracket, 2nd place in LSL 15 Silver Bracket, 3rd place in URC x LoRule — a local ru-tournament).
           <span className="section-subHeader">2022</span>
           <ReactMarkdown className="about-text">
-            More and more tournaments — and more and more good results! Our results are gradually got better. Of
-            course they were some ups and downs, but *ups amount* was higher ~(˘▾˘~).
+            As we participated in more and more tournaments we got better and better results! 
+            There were some ups and downs, but overall there were way more ups ~(˘▾˘~).
           </ReactMarkdown>
           <ReactMarkdown className="about-text">
-            In may 2022 was a very important tournament for us — LSL 20! Altough first day was a bit rough for us,
-            in day 2 we got together and **defeat everyone in Silver Bracket**! As result — we got our first money
-            prize
-            (the very fact of it was pleasant).
+            In May 2022 a very important tournament for our team was held — LSL 20! 
+            Although day 1 was a bit rough for us, on day 2 we got our act together and **won every set in Silver Bracket**! 
+            As a result we got our first money prize.
           </ReactMarkdown>
           <p className="about-text">
-            One of our global goals was to prepare for Splatoon 3. Well, it's almost there and we are ready for it!
+            One of our main goals these last two years has been to prepare for Splatoon 3. Well, with certainty we can say that we are more than ready for it!
           </p>
         </div>
         <div className="col-img">
