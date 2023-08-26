971c6aa4c0050c2a3d87b3ea076a0a960c0c27b5
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2023-08-26 11:58:56 +0300

    input filter for maps

--- src/components/Maps/MapsGallery.tsx
+++ src/components/Maps/MapsGallery.tsx
@@ -1,44 +1,65 @@
 import './MapsGallery.css'
 import bg from "../../images/Backgrounds/bg2-3.webp";
 import ImageGallery from "react-image-gallery";
+import {useState} from "react";
 
 function MapsGallery() {
 
-  const scorch = [
-    {
-      original: process.env.PUBLIC_URL + '/Maps/Scorch/Зоны.png',
-      thumbnail: process.env.PUBLIC_URL + '/Modes/zones.png',
-    },
-    {
-      original: process.env.PUBLIC_URL + '/Maps/Scorch/Башни.png',
-      thumbnail: process.env.PUBLIC_URL + '/Modes/tower.png',
-    },
-    {
-      original: process.env.PUBLIC_URL + '/Maps/Scorch/Рейн.png',
-      thumbnail: process.env.PUBLIC_URL + '/Modes/rainmaker.png',
-    },
-    {
-      original: process.env.PUBLIC_URL + '/Maps/Scorch/Клемы.png',
-      thumbnail: process.env.PUBLIC_URL + '/Modes/clam.png',
-    },
-    {
-      original: process.env.PUBLIC_URL + '/Maps/Scorch/Терфы.png',
-      thumbnail: process.env.PUBLIC_URL + '/Modes/turf.png',
-    },
+  const [map, setMap] = useState('');
+
+  const maps = [{key: 'Scorch', name: 'Опалённое Ущелье (Scorch Gorge)'},
+    {key: 'Eeltail', name: 'Угрёво-Скатово (Eeltail Alley)'},
   ];
+
+  console.log('map', map);
+  const filteredMaps = map == '' ? maps : maps.filter(item => item.name.toLowerCase().includes(map.toLowerCase()));
 
   return(
       <div className="anim-bg-block" style={{ width: '100%', backgroundImage: `url(${bg})` }}>
-        <h2>Our callouts for maps! Work in progress!</h2>
+        <h2>Our callouts for maps. Work in progress!</h2>
         <h4>Created by yaga, based on our and other people knowledge</h4>
-        <hr/>
-        <h3>Scorch Gorge</h3>
-        <div className="gallery-wrapper">
-          <ImageGallery items={scorch} />
-        </div>
-
+        <hr className="section-divider" />
+        <input value={map} name="mapName"  onChange={e => setMap(e.target.value)} />
+        {filteredMaps.map(item => (
+          <div className="gallery-wrapper" key={item.key}>
+            <h2>{item.name}</h2>
+            <ImageGallery items={generateImageGallery(item.key)} />
+          </div>))
+        }
       </div>
   );
+}
+
+function generateImageGallery(map: string) {
+  const base = process.env.PUBLIC_URL;
+  const zones = '/Modes/zones.png';
+  const tower = '/Modes/tower.png';
+  const rainmaker = '/Modes/rainmaker.png';
+  const clam = '/Modes/clam.png';
+  const turf = '/Modes/turf.png';
+
+  return [
+    {
+      original:base + `/Maps/${map}/Зоны.png`,
+      thumbnail: base + zones,
+    },
+    {
+      original: base + `/Maps/${map}/Башни.png`,
+      thumbnail: base + tower,
+    },
+    {
+      original: base + `/Maps/${map}/Рейн.png`,
+      thumbnail: base + rainmaker,
+    },
+    {
+      original: base + `/Maps/${map}/Клемы.png`,
+      thumbnail: base + clam,
+    },
+    {
+      original: base + `/Maps/${map}/Терфы.png`,
+      thumbnail: base + turf,
+    },
+  ];
 }
 
 export default MapsGallery;
